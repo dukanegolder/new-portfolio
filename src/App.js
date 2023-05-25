bebfa74e11838f5f68e2fbce63113d29abd552d5
dukanegolder <dukanegolder@gmail.com>
2023-05-24 21:32:57 -0600

    updated routes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
         <Navigation />
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/*" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
